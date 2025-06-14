035e2fbb92a6582e9c8c5e3c62be829c1416e534
animedubber <sakshamranjan2026@gmail.com>
2025-06-14 12:18:26 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
-    OWNER = config("OWNER")
+    APP_ID = config("20899529", default=6, cast=int)
+    API_HASH = config("0297693c81aac01b704702f334decddd", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = config("7550090655:AAG-sMeHLjgQSPPteGUAK8vk_o0MPs1X0gA")
+    DEV = 8002803133
+    OWNER = config("8002803133")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
